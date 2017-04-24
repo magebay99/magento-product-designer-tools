54f3471ec27d106f80652ab118d9eea5de88fa79
maiuoc <maiuoc.k8c@gmail.com>
2017-04-24 14:00:53 +0700

    fix link pdp

--- app/code/local/PDP/Integration/Helper/Data.php
+++ app/code/local/PDP/Integration/Helper/Data.php
@@ -143,5 +143,93 @@ class PDP_Integration_Helper_Data extends Mage_Core_Helper_Abstract
 		}
 		return false;
 	}
+	/**
+	* get pdp link
+	* @param $pdpSetting
+	* @return string $link
+	**/
+	function getPdpLink()
+	{
+		$pdpConfig = Mage::getStoreConfig('integration');
+		$pdpUrl = $pdpConfig['setting']['pdp_link'];
+		if($this->validate_domain_name($pdpUrl))
+		{
+			
+		}
+		else
+		{
+			$pdpUrl = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_LINK).$pdpUrl.'/';
+			$pdpUrl = str_replace('index.php/','',$pdpUrl);
+			$pdpUrl = $this->getPdpUrl($pdpUrl);
+		}
+		return $pdpUrl;
+	}
+	/**
+	 * checks if a domain name is valid
+	 * @param  string $domain_name 
+	 * @return bool              
+	 */
+	public function validate_domain_name($domain_name)
+	{
+		//FILTER_VALIDATE_URL checks length but..why not? so we dont move forward with more expensive operations
+		$domain_len = strlen($domain_name);
+		if ($domain_len < 3 OR $domain_len > 253)
+			return FALSE;
+
+		//getting rid of HTTP/S just in case was passed.
+		if(stripos($domain_name, 'http://') === 0)
+			$domain_name = substr($domain_name, 7); 
+		elseif(stripos($domain_name, 'https://') === 0)
+			$domain_name = substr($domain_name, 8);
+
+		//we dont need the www either                 
+		if(stripos($domain_name, 'www.') === 0)
+			$domain_name = substr($domain_name, 4); 
+
+		//Checking for a '.' at least, not in the beginning nor end, since http://.abcd. is reported valid
+		if(strpos($domain_name, '.') === FALSE OR $domain_name[strlen($domain_name)-1]=='.' OR $domain_name[0]=='.')
+			return FALSE;
+		//now we use the FILTER_VALIDATE_URL, concatenating http so we can use it, and return BOOL
+		return (filter_var ('http://' . $domain_name, FILTER_VALIDATE_URL)===FALSE)? FALSE:TRUE;
+
+	}
+	/**
+	* get domain name from url 
+	* @param string $url
+	* @return string $url
+	**/
+	private function getPdpUrl($domain_name)
+	{
+		$url = '';
+		$http = 'http://';
+		$okDomain = true;
+		$domain_len = strlen($domain_name);
+		if ($domain_len < 3 OR $domain_len > 253)
+		{
+			$okDomain = false;
+		}
+		//getting rid of HTTP/S just in case was passed.
+		if(stripos($domain_name, 'http://') === 0)
+			$domain_name = substr($domain_name, 7); 
+		elseif(stripos($domain_name, 'https://') === 0)
+		{
+			$domain_name = substr($domain_name, 8);
+			$http = 'https://';
+		}
+		//we dont need the www either                 
+		if(stripos($domain_name, 'www.') === 0)
+			$domain_name = substr($domain_name, 4); 
+
+		//Checking for a '.' at least, not in the beginning nor end, since http://.abcd. is reported valid
+		if(strpos($domain_name, '.') === FALSE OR $domain_name[strlen($domain_name)-1]=='.' OR $domain_name[0]=='.')
+		{
+			$okDomain = false;
+		}
+		if($okDomain)
+		{
+			$url  = $http.$domain_name;
+		}
+		return $url;
+	}
 }
 ?>
