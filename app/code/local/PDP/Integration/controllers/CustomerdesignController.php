4da84d0bc376ce5842cff066e2ef35190a1db9c5
root <root@server.dulichvnn.com>
2017-06-10 08:49:58 +0000

    test commet

--- app/code/local/PDP/Integration/controllers/CustomerdesignController.php
+++ app/code/local/PDP/Integration/controllers/CustomerdesignController.php
@@ -5,7 +5,7 @@ Class PDP_Integration_CustomerdesignController extends Mage_Core_Controller_Fron
         parent::preDispatch();
        	if (!Mage::getSingleton('customer/session')->authenticate($this)) {
             $this->setFlag('', 'no-dispatch', true);
-	       // adding message in customer login page
+	       // adding message in customer login page 
 	       Mage::getSingleton('core/session')
                 ->addError(Mage::helper('integration')->__('Please sign in or create a new account!'));
         }
